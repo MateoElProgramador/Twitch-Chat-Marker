f7e1877c76fb10bc2550acb20e73591d6196772d
MateoElProgramador <mharding2@sheffield.ac.uk>
2021-07-21 20:56:03 +0100

    Basic chat mockup, no scrolling or automated messages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import './stylesheets/App.css';
+import './stylesheets/app.sass';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatBox />
     </div>
+  );
+}
+
+function ChatBox() {
+  return (
+    <div id="chat-box">
+      <ChatMessage
+          chatterName="matashio"
+          content="Respect that"
+        />
+      <ChatMessage
+        chatterName="CactusSB"
+        content="I don't believe you"
+      />
+      <ChatMessage
+        chatterName="MateoElGoron"
+        content="Yep, it's a bold move mind"
+      />
+    </div>
+  );
+}
+
+function ChatMessage(props) {
+  let colours = ["0000FF", "FF0000", "8A2BE2", "FF69B4", "1E90FF", "008000", "00FF7F", "B22222", "DAA520", "FF4500", "2E8B57", "5F9EA0", "D2691E"];
+  let randColour = colours[Math.floor(Math.random() * colours.length)];
+  const style = {color: `#${randColour}`};
+  return (
+        <div className="chat-message">
+          <span className="chatter-name" style={style}>{props.chatterName}</span>:&nbsp;
+          <span className="chatter-content">{props.content}</span>
+        </div>
   );
 }
 
