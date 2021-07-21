924fc50d86c928c8e5658f7dc2d5cebc5b2a6c06
MateoElProgramador <mharding2@sheffield.ac.uk>
2021-07-21 22:26:26 +0100

    Add new message every 5 seconds, with consistent colours for names

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import './stylesheets/App.css';
 import './stylesheets/app.sass';
 
@@ -10,28 +11,42 @@ function App() {
 }
 
 function ChatBox() {
+
+  // Create state for functional component, storing chat data:
+  const [messages, setMessages] = useState([{chatterName: "streamlurkr", colour: "000000", content: "!lurk"}]);
+
+  // Set up interval for new message to be added to state every 5 seconds:
+  useEffect(() => {
+    const updateChat = setInterval(() => {
+      // Get random colour from list of hex codes:
+      const colours = ["0000FF", "FF0000", "8A2BE2", "FF69B4", "1E90FF", "008000", "00FF7F", "B22222", "DAA520", "FF4500", "2E8B57", "5F9EA0", "D2691E"];
+      let randColour = colours[Math.floor(Math.random() * colours.length)];
+      
+      // Create new message object and append to messages state list:
+      const newMessage = {chatterName: "NewBoi_" + Math.floor(Math.random() * 1000), colour: randColour, content: "I'm new!"};
+      setMessages([...messages, newMessage]);
+    }, 5000);
+
+    return () => clearInterval(updateChat);
+  });
+
+  let message_comps = [];   // message components
+
+  // Collate list of components containing info from the messages state list:
+  for (let i=0; i<messages.length; i++) {
+    message_comps.push(<ChatMessage key={i} chatterName={messages[i].chatterName} colour={messages[i].colour} content={messages[i].content} />);
+  }
+
   return (
     <div id="chat-box">
-      <ChatMessage
-          chatterName="matashio"
-          content="Respect that"
-        />
-      <ChatMessage
-        chatterName="CactusSB"
-        content="I don't believe you"
-      />
-      <ChatMessage
-        chatterName="MateoElGoron"
-        content="Yep, it's a bold move mind"
-      />
+      {message_comps}
     </div>
   );
 }
 
+
 function ChatMessage(props) {
-  let colours = ["0000FF", "FF0000", "8A2BE2", "FF69B4", "1E90FF", "008000", "00FF7F", "B22222", "DAA520", "FF4500", "2E8B57", "5F9EA0", "D2691E"];
-  let randColour = colours[Math.floor(Math.random() * colours.length)];
-  const style = {color: `#${randColour}`};
+  const style = {color: `#${props.colour}`};
   return (
         <div className="chat-message">
           <span className="chatter-name" style={style}>{props.chatterName}</span>:&nbsp;
