49c992d61b3290dc15f42d1372b24c81d8442656
MateoElProgramador <mharding2@sheffield.ac.uk>
2021-07-22 18:09:01 +0100

    Refactor ChatBox component (much cleaner!)

--- src/App.js
+++ src/App.js
@@ -19,18 +19,43 @@ function ChatBox() {
 
   // Set up interval for new message to be added to state every 3 seconds:
   useEffect(() => {
-    const updateChat = setInterval(() => {
-      // Get random colour from list of hex codes:
-      const colours = ["0000FF", "FF0000", "8A2BE2", "FF69B4", "1E90FF", "008000", "00FF7F", "B22222", "DAA520", "FF4500", "2E8B57", "5F9EA0", "D2691E"];
-      let randColour = colours[Math.floor(Math.random() * colours.length)];
-      
-      // Create new message object and append to messages state list:
-      const newMessage = {chatterName: "NewBoi_" + Math.floor(Math.random() * 1000), colour: randColour, content: "I'm new!"};
-      setMessages([...messages, newMessage]);
-    }, 3000);
+    const updateChat = setInterval(newFakeMessage, 3000);
 
     return () => clearInterval(updateChat);
   });
+
+
+  // Create new fake message with random name colour, and add to messages state list:
+  const newFakeMessage = () => {
+    // Get random colour from list of hex codes:
+    const colours = ["0000FF", "FF0000", "8A2BE2", "FF69B4", "1E90FF", "008000", "00FF7F", "B22222", "DAA520", "FF4500", "2E8B57", "5F9EA0", "D2691E"];
+    let randColour = colours[Math.floor(Math.random() * colours.length)];
+    
+    // Create new message object and append to messages state list:
+    const newMessage = {chatterName: "NewBoi_" + Math.floor(Math.random() * 1000), colour: randColour, content: "I'm new!"};
+    setMessages([...messages, newMessage]);
+  }
+
+  // Callback for catchup button, moves marker position to bottom of chat list:
+  const catchup = () => {
+    setMarkerInd(messages.length-1);
+    $("#chat-end")[0].scrollIntoView(false);
+    // $(".chat-marker")[0].scrollIntoView(false);
+  };
+
+  // Callback for up button, decrements marker index if it's not already 0:
+  const markerUp = () => {
+    if (markerInd !== 0) {
+      setMarkerInd(markerInd-1);
+    }
+  };
+
+  // Callback for down button, increments marker index if it's not already at the end of the messages list:
+  const markerDown = () => {
+    if (markerInd !== messages.length-1)
+      setMarkerInd(markerInd+1);
+  };
+
 
   let message_comps = [];   // message components
 
@@ -50,36 +75,19 @@ function ChatBox() {
     }
   }
 
-  message_comps.push(<span id="chat-end"/>);
+  // Add hidden element to bottom of chat box to mark end of messages:
+  message_comps.push(<span id="chat-end" key="chat-end"/>);
 
   return (
     <div id="chat-box-container">
       <div id="chat-box">
         {message_comps}
       </div>
-
-      <button id="catchup-button" onClick={() => {
-            setMarkerInd(messages.length-1);
-            $("#chat-end")[0].scrollIntoView(false);
-            // $(".chat-marker")[0].scrollIntoView(false);
-            }}>
-        Catch up</button>
-      {/* Move marker up, with boundary check to avoid overshoot */}
-      <button id="marker-up" onClick={() => {
-            if (markerInd !== 0)
-              setMarkerInd(markerInd-1);
-            }}>
-        Up
-      </button>
-      {/* Move marker down, with boundary check to avoid overshoot */}
-      <button id="marker-down" onClick={() => {
-            if (markerInd !== messages.length-1)
-              setMarkerInd(markerInd+1);
-            }}>
-        Down
-      </button>
+      
+      <button id="catchup-button" onClick={catchup}>Catch up</button>
+      <button id="marker-up" onClick={markerUp}>Up</button>
+      <button id="marker-down" onClick={markerDown}>Down</button>
     </div>
-    
   );
 }
 
