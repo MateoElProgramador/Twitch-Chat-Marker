093085e2da87b39a15d00f2c2038a0b8a1f3f1f1
MateoElProgramador <mharding2@sheffield.ac.uk>
2021-07-22 02:14:14 +0100

    Add chat marker and buttons to move it, add scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import $ from 'jquery';
 import './stylesheets/App.css';
 import './stylesheets/app.sass';
 
@@ -11,11 +12,11 @@ function App() {
 }
 
 function ChatBox() {
-
-  // Create state for functional component, storing chat data:
+  // Create state for functional component, storing chat data and index of chat marker:
   const [messages, setMessages] = useState([{chatterName: "streamlurkr", colour: "000000", content: "!lurk"}]);
+  const [markerInd, setMarkerInd] = useState(0);
 
-  // Set up interval for new message to be added to state every 5 seconds:
+  // Set up interval for new message to be added to state every 3 seconds:
   useEffect(() => {
     const updateChat = setInterval(() => {
       // Get random colour from list of hex codes:
@@ -25,7 +26,7 @@ function ChatBox() {
       // Create new message object and append to messages state list:
       const newMessage = {chatterName: "NewBoi_" + Math.floor(Math.random() * 1000), colour: randColour, content: "I'm new!"};
       setMessages([...messages, newMessage]);
-    }, 5000);
+    }, 3000);
 
     return () => clearInterval(updateChat);
   });
@@ -34,17 +35,55 @@ function ChatBox() {
 
   // Collate list of components containing info from the messages state list:
   for (let i=0; i<messages.length; i++) {
-    message_comps.push(<ChatMessage key={i} chatterName={messages[i].chatterName} colour={messages[i].colour} content={messages[i].content} />);
+    message_comps.push(<ChatMessage
+                          key={i}
+                          chatterName={messages[i].chatterName}
+                          colour={messages[i].colour}
+                          content={messages[i].content}
+                        />
+    );
+
+    // Add chat marker at correct position:
+    if (i === markerInd) {
+      message_comps.push(<hr key="marker" className="chat-marker"/>);
+    }
   }
 
+  message_comps.push(<span id="chat-end"/>);
+
   return (
-    <div id="chat-box">
-      {message_comps}
+    <div id="chat-box-container">
+      <div id="chat-box">
+        {message_comps}
+      </div>
+
+      <button id="catchup-button" onClick={() => {
+            setMarkerInd(messages.length-1);
+            $("#chat-end")[0].scrollIntoView();
+            // $(".chat-marker")[0].scrollIntoView();
+            }}>
+        Catch up</button>
+      {/* Move marker up, with boundary check to avoid overshoot */}
+      <button id="marker-up" onClick={() => {
+            if (markerInd !== 0)
+              setMarkerInd(markerInd-1);
+            }}>
+        Up
+      </button>
+      {/* Move marker down, with boundary check to avoid overshoot */}
+      <button id="marker-down" onClick={() => {
+            if (markerInd !== messages.length-1)
+              setMarkerInd(markerInd+1);
+            }}>
+        Down
+      </button>
     </div>
+    
   );
 }
 
 
+// Component for an invididual chat message:
 function ChatMessage(props) {
   const style = {color: `#${props.colour}`};
   return (
